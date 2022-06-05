0235f6c57bfe8e4b0762af4e6ac813d6567d36b7
NavarroAJona <tifon77@outlook.es>
2022-06-05 02:31:13 -0600

    Tercer metodo lista finalizado

--- pokemon.py
+++ pokemon.py
@@ -1,7 +1,8 @@
 from ctypes.wintypes import HLOCAL
 from re import A
+from turtle import pos
 import pandas as pd
-import bisect
+import numpy as np
 
 #De momento no se hace ninguna limpieza ya que no parece necesario
 def load_words(filename):
@@ -38,6 +39,40 @@ def get_sequences(words, n):
             innerRange+=1
     return sorted(sequenceList)
 
+#Potencialmente nombres feos
+def calculate_transitions(words, sequences):
+    size = len(sequences)
+    #Este sequencesSize se usa para mas adelante para
+    #poder mezclar dos secuencias y ver si estan en una palabra
+    sequencesSize = len(sequences[0])
+    if(sequencesSize==1):
+        sequencesSize=0
+    transitionMatrix = np.zeros((size,size))
+    for i in range(0,size):
+        divideValueCounter = 1
+        for j in range(0,size):
+            sequencesMerged = sequences[i] + sequences[j][sequencesSize:]
+            if(any(sequencesMerged in string for string in words)):
+                #Con esto cuento cuantas veces se repite una subbsecuencia en todas las palabras
+                sequencesCounter = [string for string in words if sequencesMerged in string]  
+                divideValueCounter +=1
+                transitionMatrix[i][j]=len(sequencesCounter)
+        for position in range(0,size):
+            if(transitionMatrix[i][position]!=0):
+                transitionMatrix[i][position]=round(transitionMatrix[i][position]/divideValueCounter,3)
+                
+    print(transitionMatrix)
+    return transitionMatrix
 
-hola= get_sequences(add_decorators(load_words("poke.csv"),"$",2),1)
-print(hola)
+#Puse "wow" para comprobar que si nos funciona bien que si una subsecuencia
+#aparece dos veces entonces salga 0.66 y 0.33, ahi si ve la explicacion
+#del metodo 3 me entendera mejor creo
+entradaEjemplo = ['$hello$','$world$','$wow$']
+palabras = load_words("poke.csv")
+decorador = "$"
+#Sustituir con palabra si queremos cargar el csv
+decoradores = add_decorators(entradaEjemplo,decorador,2)
+secuencia= get_sequences(decoradores,1)
+calculate_transitions(entradaEjemplo,secuencia)
+print(secuencia)
+
