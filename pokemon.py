6c3c5af2c96ede320260c39a693e8663d14c9401
Jonathan <52896354+NavarroAJona@users.noreply.github.com>
2022-06-08 15:45:50 -0600

    detalles

--- pokemon.py
+++ pokemon.py
@@ -3,7 +3,7 @@ from re import A
 from turtle import pos
 import numpy as np
 import random
-#De momento no se hace ninguna limpieza ya que no parece necesario
+
 def load_words(filename):
     pokemonList = []
     with open(filename, "r") as archive:
@@ -25,7 +25,6 @@ def add_decorators(words, decorator, n):
         counterList+=1
     return newList
 
-#Definitivamente podria tener mejores nombres pero es muy tarde y no lo se
 def get_sequences(words, n):
     sequenceList = []
     counterList = 0
@@ -38,7 +37,6 @@ def get_sequences(words, n):
             innerRange+=1
     return sorted(sequenceList)
 
-#Potencialmente nombres feos
 def calculate_transitions(words, sequences):
     transitionDicts = {}
     sequenceSize = len(sequences[0])
@@ -93,18 +91,66 @@ def generate_word(model,seed):
     generatedWord = generatedWord.replace("$","")
     return generatedWord.capitalize()
 
-#entradaEjemplo = ['hello','world']
-#e= ["casa"]
-#palabras = load_words("pokemon.csv")
-#ngrama=2
-#model = create_model(palabras,1)
-#print(generate_word(model, 17))
-#model = create_model(palabras,2)
-#print(generate_word(model, 42))
-#model = create_model(palabras,3)
-#print(generate_word(model, 21))
-#r = random.Random()
-#semilla =420
-#r.seed(semilla)
-#a=generate_word(create_model(entradaEjemplo,ngrama),value)
-#print(matrix,seq)
+def get_probability(model,word):
+    transition,sequences = model
+    sequenceSize = len(sequences[0])
+    possiblePokemon = add_decorators([word],"$",sequenceSize)[0].lower()
+    probability = 1
+    for position in range(0,len(possiblePokemon)):
+        subSequence1 = possiblePokemon[position:position+sequenceSize]
+        subSequence2 = possiblePokemon[position+1:position+sequenceSize+1]
+        if(len(subSequence1)==len(subSequence2)):
+            print(transition[sequences.index(subSequence1)][sequences.index(subSequence2)])
+            transitionValue = transition[sequences.index(subSequence1)][sequences.index(subSequence2)]
+            probability = probability*transitionValue
+
+    return probability
+
+
+
+seed = 5000
+palabras = load_words("pokemon.csv")
+model1 = create_model(palabras,1)
+model2 = create_model(palabras,2)
+model3 = create_model(palabras,3)
+
+
+#valor1=get_probability(create_model(palabras,1),"mew")
+#valor2=get_probability(create_model(palabras,2),"mew")
+#valor3=get_probability(create_model(palabras,3),"mew")
+#print("proba1:", valor1)
+#print("proba2:", valor2)
+#pprint("proba3:", valor3)
+
+#ojo lo podemos cambiar jejeps
+
+#PARTE 8 DEL LABORATORIO
+#a ¿Por qué la probabilidad de formar un nombre parece aumentar conforme n incrementa?
+#R/ Esto se puede deber a que, al hacer ngramas mas grandes hay menos posibles combinaciones
+#de subsecuencias, lo que hace que cada posible paso de una subsecuencia a otra sea mayor
+
+#b ¿Encontró algún otro nombre interesante para un Pokemon? ¿Con qué n y con qué semilla? 
+# ¿Encontró algún nombre que no tiene sentido? ¿Con qué n y semilla? 
+# (Incluya el código de demostración)
+
+#R/ Entre los nombres interesantes o posibles que se generaron estaban:
+# Nomperio seed 863 n 2
+print("n=1 seed 863:",generate_word(model2,863))
+# Slygon seed 878 n 2
+print("n=1 seed 878:",generate_word(model2,878))
+# Huronda seed 885 n 1
+print("n=1 seed 885:",generate_word(model1,885))
+# Marelphox seed 893 n 3
+print("n=1 seed 893:",generate_word(model3,893))
+#Entre los nombres que no tienen sentido estaban:
+# Nkiewawiopinittcrnonty seed 841 n 1
+print("n=1 seed 841:",generate_word(model1,841))
+# Excadabrawdaunchespirizionneking seed 855 n 2
+print("n=1 seed 855:",generate_word(model2,855))
+# Linachoranggupoweldeockiorsaneligro seed 2869 n 1 
+print("n=1 seed 863:",generate_word(model1,2869))
+
+#c Explique en sus propias palabras 
+# ¿de qué manera se están usando las cadenas de Markov para modelar los nombres?
+
+#R/
